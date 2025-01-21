c777f269319cfc92a4560ebd1002611264f96ea3
bbysinXcans <80329334+bbysinX606@users.noreply.github.com>
2025-01-21 16:59:50 +0700

    Create v41.sh

---
+++ v41.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+s
+echo "EXECUTOR T3RN"
+
+sleep 1
+
+cd $HOME
+rm -rf executor
+sleep 1
+sudo apt update
+sudo apt upgrade -y
+
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.41.0/executor-linux-v0.41.0.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.41.0.tar.gz"
+
+echo "Downloading the Executor binary from $EXECUTOR_URL..."
+curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
+
+if [ $? -ne 0 ]; then
+    echo "Failed to download the Executor binary. Please check your internet connection and try again."
+    exit 1
+fi
+
+echo "Extracting the binary..."
+tar -xzvf $EXECUTOR_FILE
+rm -rf $EXECUTOR_FILE
+cd executor/executor/bin
+
+echo "Binary downloaded and extracted successfully."
+echo
+
+export NODE_ENV=testnet
+
+export LOG_LEVEL=debug
+export LOG_PRETTY=false
+export EXECUTOR_PROCESS_ORDERS=true
+export EXECUTOR_PROCESS_CLAIMS=true
+
+# Prompt user for EXECUTOR_MAX_L3_GAS_PRICE
+read -p "Enter the maximum L3 gas price (EXECUTOR_MAX_L3_GAS_PRICE): " EXECUTOR_MAX_L3_GAS_PRICE
+export EXECUTOR_MAX_L3_GAS_PRICE=$EXECUTOR_MAX_L3_GAS_PRICE
+
+echo -e "EXECUTOR_MAX_L3_GAS_PRICE set to $EXECUTOR_MAX_L3_GAS_PRICE."
+echo
+
+export EXECUTOR_PROCESS_PENDING_ORDERS_FROM_API=false
+export EXECUTOR_PROCESS_ORDERS_API_ENABLED=false
+export EXECUTOR_ENABLE_BATCH_BIDDING=false
+
+# Prompt user for PRIVATE_KEY_LOCAL
+read -p "Enter your Private Key from Metamask: " PRIVATE_KEY_LOCAL
+export PRIVATE_KEY_LOCAL=$PRIVATE_KEY_LOCAL
+echo -e "\nPrivate key has been set."
+echo
+
+export ENABLED_NETWORKS='arbitrum-sepolia,base-sepolia,blast-sepolia,optimism-sepolia,l1rn'
+
+# Prompt user for KEY ALCHEMY
+read -p "Enter your Alchemy API Key (KEY ALCHEMY): " KEYALCHEMY
+
+export RPC_ENDPOINTS_ARBT="https://arb-sepolia.g.alchemy.com/v2/$KEYALCHEMY,https://api.zan.top/arb-sepolia,https://endpoints.omniatech.io/v1/arbitrum/sepolia/public,https://arbitrum-sepolia-rpc.publicnode.com"
+export RPC_ENDPOINTS_BSSP="https://base-sepolia.g.alchemy.com/v2/$KEYALCHEMY,https://base-sepolia.gateway.tenderly.co,https://sepolia.base.org,https://base-sepolia-rpc.publicnode.com"
+export RPC_ENDPOINTS_BLSS="https://blast-sepolia.g.alchemy.com/v2/$KEYALCHEMY,https://sepolia.blast.io,https://endpoints.omniatech.io/v1/blast/sepolia/public"
+export RPC_ENDPOINTS_OPSP="https://opt-sepolia.g.alchemy.com/v2/$KEYALCHEMY"
+export RPC_ENDPOINTS_L1RN='https://brn.rpc.caldera.xyz/,https://brn.calderarpc.com/'
+
+sleep 2
+echo "Starting the Executor..."
+./executor
+
+rm -rf t3rn.sh
